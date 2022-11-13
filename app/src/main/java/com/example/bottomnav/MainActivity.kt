be401ac393a4001ea9c4a021fed2d32bc6128650
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-11-13 20:58:34 +0700

    Final

---
+++ app/src/main/java/com/example/bottomnav/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.bottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bot_nav_view)
+        val navController = findNavController(R.id.fragment_view)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
